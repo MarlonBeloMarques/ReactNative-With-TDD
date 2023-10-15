a57eed63d933c5f78ed666cfb5b36a4c916114a4
Marlon Marques <marlon.belohd@gmail.com>
2023-10-15 20:41:44 -0300

    test(transfermoney): show the recipients bank account information correctly

--- tests/presentation/transferMoney/index.spec.tsx
+++ tests/presentation/transferMoney/index.spec.tsx
@@ -10,6 +10,9 @@ describe('Presentation: TransferMoney', () => {
         agency={''}
         currentAccount={''}
         photoProfileOfAccount={''}
+        recipientUserAccountName={''}
+        recipientAgency={''}
+        recipientCurrentAccount={''}
       />,
     );
 
@@ -28,14 +31,17 @@ describe('Presentation: TransferMoney', () => {
         agency={agencyFaker}
         currentAccount={currentAccountFaker}
         photoProfileOfAccount={''}
+        recipientUserAccountName={''}
+        recipientAgency={''}
+        recipientCurrentAccount={''}
       />,
     );
 
-    const nameAccountUser = getByTestId('name_account_user_id');
-    const agencyLabel = getByTestId('agency_label_id');
-    const agency = getByTestId('agency_id');
-    const currentAccountLabel = getByTestId('current_account_label_id');
-    const currentAccount = getByTestId('current_account_id');
+    const nameAccountUser = getByTestId('name_account_user_1_id');
+    const agencyLabel = getByTestId('agency_label_1_id');
+    const agency = getByTestId('agency_1_id');
+    const currentAccountLabel = getByTestId('current_account_label_1_id');
+    const currentAccount = getByTestId('current_account_1_id');
 
     expect(nameAccountUser.props.children).toEqual(nameFaker);
     expect(agencyLabel.props.children).toEqual('Agency');
@@ -55,10 +61,13 @@ describe('Presentation: TransferMoney', () => {
         agency={agencyFaker}
         currentAccount={currentAccountFaker}
         photoProfileOfAccount={photoProfile}
+        recipientUserAccountName={''}
+        recipientAgency={''}
+        recipientCurrentAccount={''}
       />,
     );
 
-    expect(getByTestId('photo_profile_of_account_id').props.source).toEqual({
+    expect(getByTestId('photo_profile_of_account_1_id').props.source).toEqual({
       uri: photoProfile,
     });
   });
@@ -74,10 +83,42 @@ describe('Presentation: TransferMoney', () => {
         agency={agencyFaker}
         currentAccount={currentAccountFaker}
         photoProfileOfAccount={photoProfile}
+        recipientUserAccountName={''}
+        recipientAgency={''}
+        recipientCurrentAccount={''}
       />,
     );
 
     expect(getByTestId('to_id').props.children).toEqual('To');
+  });
+
+  test('should show the recipients bank account information correctly', () => {
+    const nameFaker = faker.person.firstName();
+    const agencyFaker = faker.finance.accountNumber();
+    const currentAccountFaker = faker.finance.accountNumber();
+    const {getByTestId} = render(
+      <TransferMoney
+        nameAccountUser={''}
+        agency={''}
+        currentAccount={''}
+        photoProfileOfAccount={''}
+        recipientUserAccountName={nameFaker}
+        recipientAgency={agencyFaker}
+        recipientCurrentAccount={currentAccountFaker}
+      />,
+    );
+
+    const nameAccountUser = getByTestId('name_account_user_2_id');
+    const agencyLabel = getByTestId('agency_label_2_id');
+    const agency = getByTestId('agency_2_id');
+    const currentAccountLabel = getByTestId('current_account_label_2_id');
+    const currentAccount = getByTestId('current_account_2_id');
+
+    expect(nameAccountUser.props.children).toEqual(nameFaker);
+    expect(agencyLabel.props.children).toEqual('Agency');
+    expect(agency.props.children).toEqual(agencyFaker);
+    expect(currentAccountLabel.props.children).toEqual('Current Account');
+    expect(currentAccount.props.children).toEqual(currentAccountFaker);
   });
 });
 
@@ -86,6 +127,9 @@ type Props = {
   agency: string;
   currentAccount: string;
   photoProfileOfAccount: string;
+  recipientUserAccountName: string;
+  recipientAgency: string;
+  recipientCurrentAccount: string;
 };
 
 const TransferMoney = ({
@@ -93,22 +137,34 @@ const TransferMoney = ({
   agency,
   currentAccount,
   photoProfileOfAccount,
+  recipientAgency,
+  recipientCurrentAccount,
+  recipientUserAccountName,
 }: Props) => {
   return (
     <View>
       <Text testID="title_id">Transfer</Text>
       <AccountCard
+        id="1"
         nameAccountUser={nameAccountUser}
         agency={agency}
         currentAccount={currentAccount}
         photoProfileOfAccount={photoProfileOfAccount}
       />
       <Text testID="to_id">To</Text>
+      <AccountCard
+        id="2"
+        nameAccountUser={recipientUserAccountName}
+        agency={recipientAgency}
+        currentAccount={recipientCurrentAccount}
+        photoProfileOfAccount={''}
+      />
     </View>
   );
 };
 
 type AccountCardProps = {
+  id: string;
   nameAccountUser: string;
   agency: string;
   currentAccount: string;
@@ -116,6 +172,7 @@ type AccountCardProps = {
 };
 
 const AccountCard = ({
+  id,
   nameAccountUser,
   agency,
   currentAccount,
@@ -123,13 +180,13 @@ const AccountCard = ({
 }: AccountCardProps) => (
   <View>
     <Image
-      testID="photo_profile_of_account_id"
+      testID={`photo_profile_of_account_${id}_id`}
       source={{uri: photoProfileOfAccount}}
     />
-    <Text testID="name_account_user_id">{nameAccountUser}</Text>
-    <Text testID="agency_label_id">{'Agency'}</Text>
-    <Text testID="agency_id">{agency}</Text>
-    <Text testID="current_account_label_id">{'Current Account'}</Text>
-    <Text testID="current_account_id">{currentAccount}</Text>
+    <Text testID={`name_account_user_${id}_id`}>{nameAccountUser}</Text>
+    <Text testID={`agency_label_${id}_id`}>{'Agency'}</Text>
+    <Text testID={`agency_${id}_id`}>{agency}</Text>
+    <Text testID={`current_account_label_${id}_id`}>{'Current Account'}</Text>
+    <Text testID={`current_account_${id}_id`}>{currentAccount}</Text>
   </View>
 );
